206f65654c73fdc73ce39d08018f55bd7fa87ccb
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-18 14:31:34 -0600

    Redirect to login page if clicked favorite without being signed in
    Co-Authored-by: Evan Markowitz <emarkowitz1@gmail.com>
    Co-Authored-by: Andreea Hanson <andreea.hanson7@gmail.com>

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router'; 
 import './MovieCard.css';
 
-const MovieCard = ({moviePoster}) => {
-  const url = 'http://image.tmdb.org/t/p/w154'
- return (
-   <article>
-     <img src={`${url}${moviePoster}`} alt=''/>
-   </article>
- )
+class MovieCard extends Component {
+  constructor({moviePoster, user}){
+    super({moviePoster, user});
+    this.state = {
+      url: 'http://image.tmdb.org/t/p/w154',
+      redirect: false
+    }
+  }
+
+  clickFav = () => { 
+    if(!this.props.user.name){
+      this.setState({redirect:!this.state.redirect})
+    }
+   }
+
+  
+  
+  render() {
+    return (
+      <article>
+        <img src={`${this.state.url}${this.props.moviePoster}`} alt=''/>
+        <button onClick={this.clickFav}>Favorite</button>
+        {this.state.redirect && <Redirect to='/login'/> }
+      </article>
+    )
+  }
 }
 
  
-export default MovieCard;
+const mapStateToProps = (state) => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps)(MovieCard)
