aec8ddcef54af3f52388f0640d05a443563622bf
Andreea Hanson <andreea.hanson7@gmail.com>
2019-07-21 12:48:59 -0600

    Finish snapshot tests except for dispatchToProps in signup and login forms
    Co-Authored-by: Evan Markowitz <emarkowitz1@gmail.com>
    Co-Authored-by: Jacqueline Moore <jacquelinebmoore@gmail.com>
    Co-Authored-by: Andreea Hanson <andreea.hanson7@gmail.com>

--- src/containers/SignUpForm/SignUpForm.test.js
+++ src/containers/SignUpForm/SignUpForm.test.js
@@ -1,4 +1,44 @@
+import { SignUpForm, mapDispatchToProps, mapStateToProps } from './SignUpForm';
+import { setLoggedInUser } from '../../actions';
 import React from 'react';
-import NavBar from './NavBar';
 import { shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
+
+describe('SignUpFormContainer', () => {
+    describe('mapStateToProps', () => {
+        it('should', () => {
+            const state = {isLoggedIn: false};
+            
+            const mappedProps = mapStateToProps(state);
+
+            expect(mappedProps).toEqual(state);
+        })
+    });
+
+    describe('mapDispatchToProps', () => {
+        it.skip('should dispatch with a setLoggedInUser action when handleSubmit is called', () => {
+            const mockDispatch = jest.fn();
+            const mockUser = {name: 'Evan'};
+            const mockAction = setLoggedInUser(mockUser);
+
+            const mappedProps = mapDispatchToProps(mockDispatch);
+            mappedProps.handleSubmit(mockUser);
+
+            expect(mockDispatch).toHaveBeenCalledWith(mockAction);
+        })
+    })
+    describe('SignUpForm', () => {
+        let wrapper;
+        const initialState = [];
+        let store
+        const mockStore = configureStore();
+        beforeEach(() => {
+            store = mockStore(initialState);
+            wrapper = shallow(<SignUpForm store={store}/>)
+        })
+        it('should match the snapshot', () => {
+            expect(wrapper).toMatchSnapshot()
+        })
+    })
+
+})
